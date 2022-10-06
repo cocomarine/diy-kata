36e2f77ebe1296799d29d195398a335bffca408d
HJ Kang <hkang07@gmail.com>
2022-10-06 11:25:30 +0100

    reachDestination

--- src/kata5.reachDestination.js
+++ src/kata5.reachDestination.js
@@ -1,3 +1,14 @@
-const reachDestination = (distance, speed) => {};
+const reachDestination = (distance, speed) => {
+    let etA = distance / speed;
+    let intDigit = Math.trunc(etA);
+    let fracDigit = etA - intDigit;
+
+    if (fracDigit < 0.5) {
+        etA = intDigit + 0.5;
+    } else if (fracDigit > 0.5) {
+        eta = intDigit + 1;
+    }
+    return `I should be there in ${etA} hours.`;
+};
 
 module.exports = reachDestination;
--- test/kata5.reachDestination.test.js
+++ test/kata5.reachDestination.test.js
@@ -1,5 +1,16 @@
 const { reachDestination } = require("../src");
 
 describe("reachDestination", () => {
-  test("returns string with estimated time of arrival", () => {});
+  test("returns string with estimated time of arrival", () => {
+    expect(reachDestination(44, 10)).toEqual("I should be there in 4.5 hours.");
+  });
+
+  test("returns string with estimated time of arrival", () => {
+    expect(reachDestination(2, 10)).toEqual("I should be there in 0.5 hours.");
+  });
+
+  test("returns string with estimated time of arrival", () => {
+    expect(reachDestination(121, 10)).toEqual("I should be there in 12.5 hours.");
+  });
+
 });
