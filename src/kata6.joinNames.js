6fe40c885bebbedf8512bd62259cf288a7ba5671
HJ Kang <hkang07@gmail.com>
2022-10-06 12:25:25 +0100

    joinNames

--- src/kata6.joinNames.js
+++ src/kata6.joinNames.js
@@ -1,3 +1,17 @@
-const joinNames = namesObj => {};
+const joinNames = namesObj => {
+    
+    if (namesObj.length == 1) {
+        return namesObj[0].name;
+    }
+    
+    let allNames = [];
+    let lastItem = namesObj.pop();
+
+    namesObj.forEach((item) => {
+        allNames.push(item.name);
+    });
+
+    return allNames.join(', ') + " & " + lastItem.name;
+};
 
 module.exports = joinNames;
--- test/kata6.joinNames.test.js
+++ test/kata6.joinNames.test.js
@@ -1,5 +1,34 @@
 const { joinNames } = require("../src");
 
 describe("joinNames", () => {
-  test("returns string of names, seperated by commas and an ampersand", () => {});
+  test("returns string of names, seperated by commas and an ampersand", () => {
+    expect(joinNames([{
+      name: 'Bart'
+    }, {
+      name: 'Lisa'
+    }, {
+      name: 'Maggie'
+    }])).toEqual('Bart, Lisa & Maggie');
+  });
+
+  test("returns string of names, seperated by commas and an ampersand", () => {
+    expect(joinNames([{
+      name: 'Bart'
+    }, {
+      name: 'Lisa'
+    }, {
+      name: 'Maggie'
+    }, {
+      name: 'Annabel'
+    }, {
+      name: 'Mike'
+    }])).toEqual('Bart, Lisa, Maggie, Annabel & Mike');
+  });
+
+  test("returns string of names, seperated by commas and an ampersand", () => {
+    expect(joinNames([{
+      name: 'Bart'
+    }])).toEqual('Bart');
+  });
+
 });
