37b444304933afc1f8174f4863701431e914ba2f
HJ Kang <hkang07@gmail.com>
2022-10-06 09:17:17 +0100

    fizzBuss and booleanToWord

--- src/kata1.fizzBuzz.js
+++ src/kata1.fizzBuzz.js
@@ -1,3 +1,17 @@
-const fizzBuzz = number => {};
+const fizzBuzz = number => {
+    let result = "";
+
+    if (number % 3 === 0) {
+        result += "Fizz";
+    }
+    if (number % 5 === 0) {
+        result += "Buzz"
+    }
+    if (number % 3 != 0 && number % 5 !=0) {
+        return number;
+    } else {
+        return result;
+    }
+};
 
 module.exports = fizzBuzz;
--- test/kata2.booleanToWord.test.js
+++ test/kata2.booleanToWord.test.js
@@ -1,5 +1,11 @@
 const { booleanToWord } = require("../src");
 
 describe("booleanToWord", () => {
-  // how do we create specs again???
+  test("return Yes when passsed true", () => {
+    expect(booleanToWord(1)).toEqual('Yes');
+  });
+
+  test("retrun No when passed false", () => {
+    expect(booleanToWord(5)).toEqual('No');
+  });
 });
