b502915b18dd50c8028123cf0615e2173dd9ce58
HJ Kang <hkang07@gmail.com>
2022-10-06 20:20:46 +0100

    getEmployerRole

--- src/kata7.getEmployerRole.js
+++ src/kata7.getEmployerRole.js
@@ -1,3 +1,6 @@
-const getEmployerRole = (employeeName, employees) => {};
+const getEmployerRole = (employeeName, employees) => {
+    const matchingItem = employees.filter(x => x.name == employeeName);
+    return matchingItem[0].role;
+};
 
 module.exports = getEmployerRole;
--- test/kata7.getEmployerRole.test.js
+++ test/kata7.getEmployerRole.test.js
@@ -1,5 +1,29 @@
 const { getEmployerRole } = require("../src");
 
 describe("getEmployerRole", () => {
-  test("returns the employee's role in the company", () => {});
+  test("returns the employee's role in the company", () => {
+    expect(getEmployerRole('Javid', [{
+      name: 'Satti',
+      role: 'Developer'
+    }, {
+      name: 'Jenny',
+      role: 'Sales Associate'
+    }, {
+      name: 'Javid',
+      role: 'Human Recommended Reading Assistant'
+    }])).toEqual('Human Recommended Reading Assistant');
+  });
+
+  test("returns the employee's role in the company", () => {
+    expect(getEmployerRole('Satti', [{
+      name: 'Satti',
+      role: 'Developer'
+    }, {
+      name: 'Jenny',
+      role: 'Sales Associate'
+    }, {
+      name: 'Javid',
+      role: 'Human Recommended Reading Assistant'
+    }])).toEqual('Developer');
+  });
 });
